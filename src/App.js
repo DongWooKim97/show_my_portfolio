216c33e02547ebffe871fe9d07d2bf41a83a34fe
DongWooKim97 <nanaukim@naver.com>
2022-11-30 21:39:44 +0900

    feat: 임시 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import { Route, Routes } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
-	return <div>제발 되거라 ㅜㅜㅜ</div>;
+	return (
+		<>
+			<Routes>
+				<Route path="/show_my_portfolio" element={<Home />} />
+				<Route path="/show_my_portfolio/about" element={<About />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
