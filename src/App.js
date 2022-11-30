1c59219558ad5b94a35ad141eb3db921dd50f627
DongWooKim97 <nanaukim@naver.com>
2022-11-30 22:05:38 +0900

    style: 오타 수정(각 라우트별 이름 변경)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
 	return (
@@ -8,9 +11,9 @@ function App() {
 			<Routes>
 				<Route path="/show_my_portfolio" element={<Home />} />
 				<Route path="/show_my_portfolio/about" element={<About />} />
-				<Route path="/show_my_portfolio/skills" element={<About />} />
-				<Route path="/show_my_portfolio/projects" element={<About />} />
-				<Route path="/show_my_portfolio/contact" element={<About />} />
+				<Route path="/show_my_portfolio/skills" element={<Skills />} />
+				<Route path="/show_my_portfolio/projects" element={<Projects />} />
+				<Route path="/show_my_portfolio/contact" element={<Contact />} />
 			</Routes>
 		</>
 	);
