8340c184a9db3903a3aeb431e8791c0c36c66861
DongWooKim97 <nanaukim@naver.com>
2022-12-08 13:30:33 +0900

    feat: 각 페이지별 라우팅 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Skills from './pages/Skills';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/sKills" element={<Skills />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/contact" element={<Contact />} />
 		</Routes>
 	);
 }
